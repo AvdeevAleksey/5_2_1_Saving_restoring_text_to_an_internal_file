385eb36bfe51b0d607afa2589f5bbde2b978bbd9
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-11-20 14:16:07 +0300

    Initial commit

---
+++ app/src/test/java/ru/avdeev/android/a5_2_1_saving_restoring_text_to_an_internal_file/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ru.avdeev.android.a5_2_1_saving_restoring_text_to_an_internal_file;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
